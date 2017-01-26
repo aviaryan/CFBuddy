cfa786a10dfd4bf20c5124c01f161b4de7a713be
Avi Aryan <avi.aryan123@gmail.com>
2017-01-26 18:48:27 +0530

    feat: created blog detail activity

--- app/src/main/java/in/aviaryan/cfbuddy/adapter/RecentBlogsAdapter.java
+++ app/src/main/java/in/aviaryan/cfbuddy/adapter/RecentBlogsAdapter.java
@@ -1,6 +1,7 @@
 package in.aviaryan.cfbuddy.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -9,12 +10,14 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import org.parceler.Parcels;
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
 import in.aviaryan.cfbuddy.R;
 import in.aviaryan.cfbuddy.model.Blog;
+import in.aviaryan.cfbuddy.ui.BlogDetailActivity;
 
 // Thanks
 // http://www.androidhive.info/2016/05/android-working-with-card-view-and-recycler-view/
@@ -58,14 +61,17 @@ public class RecentBlogsAdapter extends RecyclerView.Adapter<RecentBlogsAdapter.
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-        Blog blog = blogs.get(position);
+        final Blog blog = blogs.get(position);
         holder.title.setText(blog.title);
         holder.author.setText(blog.handle);
         holder.time.setText(blog.time + "");
         holder.cardItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d(TAG, ((TextView) view.findViewById(R.id.rb_blog_title)).getText() + "");
+                Log.d(TAG, blog.title);
+                Intent intent = new Intent(mContext, BlogDetailActivity.class);
+                intent.putExtra("blog", Parcels.wrap(blog));
+                mContext.startActivity(intent);
             }
         });
     }
--- app/src/main/java/in/aviaryan/cfbuddy/model/Blog.java
+++ app/src/main/java/in/aviaryan/cfbuddy/model/Blog.java
@@ -1,11 +1,18 @@
 package in.aviaryan.cfbuddy.model;
 
+import org.parceler.Parcel;
+
 import java.util.Date;
 
+@Parcel
 public class Blog {
     public int id;
     public String handle;
     public String title;
     public String text;
     public Date time;
+
+    // required by Parceler
+    public Blog(){
+    }
 }
