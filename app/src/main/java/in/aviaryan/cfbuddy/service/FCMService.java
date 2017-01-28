46101a09ecb6d0ab6288b07517513b8517b774f5
Avi Aryan <avi.aryan123@gmail.com>
2017-01-28 19:43:23 +0530

    chore: show notification through FCM service

--- app/src/main/java/in/aviaryan/cfbuddy/service/FCMService.java
+++ app/src/main/java/in/aviaryan/cfbuddy/service/FCMService.java
@@ -1,5 +1,12 @@
 package in.aviaryan.cfbuddy.service;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -7,6 +14,9 @@ import com.google.firebase.messaging.RemoteMessage;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import in.aviaryan.cfbuddy.R;
+import in.aviaryan.cfbuddy.ui.MainActivity;
 
 public class FCMService extends FirebaseMessagingService {
     private String TAG = "CFLOG_FCM";
@@ -20,5 +30,28 @@ public class FCMService extends FirebaseMessagingService {
         if (map.containsKey("probId")){
             Log.d(TAG, map.get("probId"));
         }
+        sendNotification(remoteMessage.getNotification().getBody());
+    }
+
+    // https://www.simplifiedcoding.net/firebase-cloud-messaging-tutorial-android/
+    private void sendNotification(String messageBody) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Codeforces Problem of the day")
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
     }
 }
